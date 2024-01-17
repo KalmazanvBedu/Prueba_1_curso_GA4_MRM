38320ab7bb26a8ad9183f82c01c3bddeebfbcd6e
KalmazanvBedu <qacalidadalmazan@gmail.com>
2024-01-17 00:18:10 -0600

    Se cambia el mensaje asociado al clic en el botón

--- SCRIPT/script.js
+++ SCRIPT/script.js
@@ -3,5 +3,5 @@ var formButton = document.getElementById("btn-form");
 formButton.addEventListener("click",saludo);
 
 function saludo(){
-    alert("Hiciste clic en el botón 😁");
+    alert("Hola 😀 a todos (as)");
 }
